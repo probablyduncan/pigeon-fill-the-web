049af64c541d86d04efa9f6b241d3f2b47d6ea3c
probablyduncan <duncanpetrie1@gmail.com>
2025-08-01 11:21:28 +0100

    move work to index

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,4 @@
+import "./style.css"
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { ScrollSmoother } from 'gsap/ScrollSmoother'
@@ -14,23 +15,11 @@ ScrollTrigger.defaults({
     markers: true,
 })
 
-// ScrollTrigger.create({
-//     id: "those-were-the-days",
-//     trigger: "section#thosewerethedays",
-//     start: "center center-=300",
-//     end: "center top-=1000",
-//     pin: true,
-//     scrub: true,
-//     animation: gsap.to("section#thosewerethedays p", {
-//         y: -200,
-//     }),
-// })
-
 ScrollTrigger.create({
     id: "old-new",
     trigger: "section#now",
     start: "top top",
-    end: "bottom top-=700",
+    end: "bottom top",
     pin: true,
     scrub: 0.5,
     animation: gsap.timeline().to("section#now .new", {
