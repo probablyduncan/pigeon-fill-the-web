44189303c6ef95655d767082766ce2ef0a166138
probablyduncan <duncanpetrie1@gmail.com>
2025-08-04 19:59:00 +0100

    margin collapse was the problem! yippeee

--- src/script.ts
+++ src/script.ts
@@ -1,53 +1,52 @@
 import "./style.css"
-import "./shader"
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { ScrollSmoother } from 'gsap/ScrollSmoother'
+// import "./shader"
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+import { gsap } from "gsap";
+import { RoughEase } from "gsap/EasePack";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 
-ScrollSmoother.create({
-    content: "main",
-    smooth: 0.5,
-    effects: true,
-});
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, RoughEase);
 
-ScrollTrigger.defaults({
-    // markers: true,
-})
+document.addEventListener("DOMContentLoaded", () => {
+    ScrollSmoother.create({
+        content: "main",
+        smooth: 0.5,
+        effects: true,
+    });
 
-ScrollTrigger.create({
-    id: "old-new",
-    trigger: "section#now",
-    start: "top top",
-    end: "bottom top",
-    pin: true,
-    scrub: 0.5,
-    animation: gsap.timeline().to("section#now .new", {
-        opacity: 1,
-        ease: "power1.out",
-        delay: 0.1,
-    }, 0).to("section#now .old", {
-        delay: 0.1,
-        opacity: 0,
-        ease: "power1.out",
-    }, 0),
-})
+    const roughEase = "rough({template: elastic.inOut, points: 100, randomize:true, clamp:true})";
 
-ScrollTrigger.create({
-    id: "ai-1",
-    trigger: "section#goneplenty",
-    start: "top top",
-    end: "bottom top",
-    // pin: "section#goneplenty .img-wrapper",
-    scrub: 0.5,
-    animation: gsap.timeline().to("section#goneplenty img.ai", {
-        opacity: 1,
-        ease: "power1.out",
-        delay: 0.1,
-    }).to("section#goneplenty img.empty", {
-        opacity: 0,
-        ease: "power1.out",
-        delay: 0.1,
+    ScrollTrigger.create({
+        id: "old-new",
+        trigger: "section#now",
+        start: "center center",
+        end: "center+=100 center",
+        scrub: 0.5,
+        animation: gsap.timeline().to("section#now .new", {
+            opacity: 1,
+            ease: "power1.out",
+        }, 0).to("section#now .old", {
+            ease: "power1.out",
+            opacity: 0,
+        }, 0),
     })
+
+    ScrollTrigger.create({
+        id: "ai-1",
+        trigger: "section#goneplenty img.ai",
+        start: "center center",
+        end: "center center",
+        toggleActions: "play none reverse none",
+        animation: gsap.timeline().to("section#goneplenty img.ai", {
+            opacity: 1,
+            ease: roughEase,
+            duration: 0.5,
+        }).to("section#goneplenty img.empty", {
+            opacity: 0,
+            ease: roughEase,
+            duration: 0.5,
+        })
+    })
+
 })
